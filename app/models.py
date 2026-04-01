5acc274101f4893dad9970d2476eeffb5b34bb8e
vinay <uchihaitach11329@gamil.com>
2026-04-01 23:34:29 +0530

    inserted new model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=50)
+    price = models.IntegerField()
+    mileage = models.FloatField()
+    seating = models.IntegerField()
+    fuel_type = models.CharField(max_length=20)
+    transmission = models.CharField(max_length=20)
+    horsepower = models.IntegerField()
+    body_type = models.CharField(max_length=20)
+    description = models.TextField()
+    image = models.ImageField(upload_to='cars/')
+
+
+class UseCase(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Orders(models.Model):
+    pass
+
+class AddCart(models.Model):
+    pass
+
+
